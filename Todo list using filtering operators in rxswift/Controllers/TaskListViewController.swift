6e12ae6f48ea4e62f7e87637df8f5f34b8a34b05
kairzhan <kairzhankural@gmail.com>
2021-02-25 10:56:49 +0600

    design task list

---
+++ Todo list using filtering operators in rxswift/Controllers/TaskListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TaskListViewController.swift
+//  Todo list using filtering operators in rxswift
+//
+//  Created by kairzhan on 2/24/21.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
